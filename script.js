6eb61fc8d2cb0cf45b2a91d4aee90f99a3b39547
MuqtasidRoziq <muhammadmuqtasidr@gmail.com>
2026-02-25 22:48:55 +0700

    fixing responsive content

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-const Humberger = document.getElementById("humberger");
+const humberger = document.getElementById("humberger");
 const navList = document.getElementById("nav-list")
 
+humberger.addEventListener('click', function(){
+    navList.classList.toggle('active');
+})
